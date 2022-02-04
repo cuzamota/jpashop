d2657b1436756ef1cdcd5d995f1ccf18c783eef5
cuzamota <kimjs3061@naver.com>
2022-02-05 00:56:47 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화
    Query: 루트 1번, 컬렉션 1번
    ToOne 관계들을 먼저 조회하고, 여기서 얻은 식별자 orderId로 ToMany 관계인 OrderItem 을
    한꺼번에 조회
    MAP을 사용해서 매칭 성능 향상(O(1))

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -47,6 +47,12 @@ public class OrderApiController {
 
     private final OrderQueryRepository orderQueryRepository;
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
